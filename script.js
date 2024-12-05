86ace487e8ccf87c59b0d57e1543fc52eef5887a
yanasaputra18 <yanasaputra152@gmail.com>
2024-12-05 20:21:01 +0700

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+const display = document.getElementById("display");
+
+function appendToDisplay(input) {
+  display.value += input;
+}
+
+function clearDisplay() {
+  display.value = "";
+}
+
+function calculate() {
+  try {
+    display.value = eval(display.value);
+  } catch (error) {
+    display.value = "Error";
+  }
+}
+
+function deleteLastCharacter() {
+  const display = document.getElementById("display");
+  display.value = display.value.slice(0, -1);
+}
